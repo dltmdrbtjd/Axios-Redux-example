2b6c635bc4e0995ee4e7574eb852644887b1fad5
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-08 21:22:31 +0900

    Feat: create axios instance api modules

---
+++ src/lib/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  // 기본적으로 우리가 바라볼 서버의 주소
+  baseURL: 'http://localhost:4000/',
+  // headers에는 나중에 토큰을 넣어도되고 원하는 방식으로 활용해주시면 됩니다.
+  headers: {
+    'content-type': 'application/json;charset=UTF-8',
+    accept: 'application/json',
+  },
+});
+
+export const apis = {
+  // 게시물 불러오기
+  getPost: () => instance.get('/posts'),
+  // 게시물 작성하기
+  createPost: (contents) => instance.post('/posts', contents),
+  // 게시물 수정하기
+  editPost: (id, content) => instance.put(`/posts/${id}`, content),
+  // 게시물 삭제하기
+  delPost: (id) => instance.delete(`/posts/${id}`),
+};
