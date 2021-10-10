c4e4092061f04445d02cfe0cf3a44a17e2c9f2dd
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-10 16:50:25 +0900

    Feat: Add postCreate func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { postCreators } from './redux/post';
@@ -6,6 +6,23 @@ import { postCreators } from './redux/post';
 function App() {
   const dispatch = useDispatch();
   const post = useSelector((state) => state.post.list);
+
+  const [title, setTitle] = useState('');
+  const [subTitle, setSubTtitle] = useState('');
+
+  function TextInput(e, setState) {
+    setState(e.target.value);
+  }
+
+  function CreatePost() {
+    const content = {
+      id: post.length + 1,
+      title,
+      body: subTitle,
+    };
+
+    dispatch(postCreators.addPostMiddleware(content));
+  }
 
   useEffect(() => {
     dispatch(postCreators.getPostMiddleware());
@@ -22,6 +39,13 @@ function App() {
             </div>
           );
         })}
+      <div>
+        <input type='text' onChange={(e) => TextInput(e, setTitle)} />
+        <br />
+        <input type='text' onChange={(e) => TextInput(e, setSubTtitle)} />
+        <br />
+        <button onClick={CreatePost}>작성하기</button>
+      </div>
     </div>
   );
 }
